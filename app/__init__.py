16572f267b8c1de2da6913b8aebd43e2f44254fd
DiliLearngent <2193276981@qq.com>
2022-01-18 12:11:55 +0800

    '错误处理'

--- app/__init__.py
+++ app/__init__.py
@@ -1,11 +1,14 @@
 #-*- coding:utf-8 -*-
 #从flask包中导入Flask类
+import logging
 from flask import Flask,render_template
 #导入自定义类配置模块
 from config import Config
 from flask_sqlalchemy import SQLAlchemy
 from flask_migrate import Migrate
 from flask_login import LoginManager
+from logging.handlers import RotatingFileHandler
+import os
 
 
 #创建Flask实例
@@ -25,3 +28,15 @@ login.login_view = 'login'
 from app import routes
 from app import forms
 from app import models
+from app import errors
+
+if not app.debug:
+    if not os.path.exists('logs'):
+        os.mkdir('logs')
+    file_handler = RotatingFileHandler('logs/microblog.log',maxBytes=10240,backupCount=10)
+    file_handler.setFormatter(logging.Formatter('%(asctime)s %(levelname)s: %(message)s [in %(pathname)s:%(lineno)d]'))
+    file_handler.setLevel(logging.INFO)
+    app.logger.addHandler(file_handler)
+    app.logger.setLevel(logging.INFO)
+    app.logger.info('Microblog startup')
+
