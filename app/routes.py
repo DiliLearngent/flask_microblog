ffcfc1333f567c337aef3bc3895e1408d853541e
DiliLearngent <2193276981@qq.com>
2022-01-28 23:00:17 +0800

    添加分页功能

--- app/routes.py
+++ app/routes.py
@@ -2,16 +2,18 @@
 #从app包中导入app这个实例
 from datetime import datetime
 from flask import render_template,flash,redirect,url_for,request
+from sqlalchemy import false
 from werkzeug.urls import url_parse
 from app import app,db
-from app.forms import LoginForm, RegisterForm,EditProfileForm,EmptyForm
+from app.forms import LoginForm, RegisterForm,EditProfileForm,EmptyForm,PostForm
 from flask_login import current_user,login_user,logout_user,login_required
+# from flask_sqlalchemy import Pagination
 
-from app.models import User
+from app.models import User,Post
 
 #2个路由
-@app.route('/')
-@app.route('/index')
+@app.route('/',methods=['GET','POST'])
+@app.route('/index',methods=['GET','POST'])
 @login_required
 #一个视图函数
 #首页视图
@@ -28,7 +30,19 @@ def index():
     #     }
     # ]
     #return render_template('index.html',title='Home',user=user,posts=posts)
-    return render_template('index.html',title='Home')
+    form = PostForm()
+    if form.validate_on_submit():
+        post = Post(body=form.post.data,author=current_user)
+        db.session.add(post)
+        db.session.commit()
+        flash('Your post is now live!')
+        return redirect(url_for('index'))
+    page = request.args.get('page',1,int)
+    # posts = current_user.followed_posts().all()
+    posts = current_user.followed_posts().paginate(page,app.config['POSTS_PER_PAGE'],False)
+    next_url = url_for('index',page=posts.next_num) if posts.has_next else None
+    prev_url = url_for('index',page=posts.prev_num) if posts.has_prev else None
+    return render_template('index.html',title='Home Page',form=form,posts=posts.items,next_url=next_url,prev_url=prev_url)
 
 #用户登陆视图
 @app.route('/login',methods=['GET','POST'])
@@ -77,12 +91,16 @@ def register():
 @login_required
 def user(username):
     user = User.query.filter_by(username=username).first_or_404()
-    posts = [
-        {'author':user, 'body':'Test post #1'},
-        {'author':user, 'body':'Test post #2'}
-    ]
+    # posts = [
+    #     {'author':user, 'body':'Test post #1'},
+    #     {'author':user, 'body':'Test post #2'}
+    # ]
     form = EmptyForm()
-    return render_template('user.html',user=user,posts=posts,form=form)
+    page = request.args.get('page',1,int)
+    posts = user.posts.order_by(Post.timestamp.desc()).paginate(page,app.config['POSTS_PER_PAGE'],False)
+    next_url = url_for('user',username=username,page=posts.next_num) if posts.has_next else None
+    prev_url  = url_for('user',username=username,page=posts.prev_num) if posts.has_prev else None
+    return render_template('user.html',user=user,posts=posts.items,form=form,next_url=next_url,prev_url=prev_url)
 
 #记录访问时间
 @app.before_request
@@ -148,4 +166,15 @@ def unfollow(username):
         return redirect(url_for('user',username=username))
     else:
         return redirect(url_for('index'))
+
+#轻松地找到用户和关注
+@app.route('/explore')
+@login_required
+def explore():
+    # posts = Post.query.order_by(Post.timestamp.desc()).all()
+    page = request.args.get('page',1,int)
+    posts = current_user.followed_posts().paginate(page,app.config['POSTS_PER_PAGE'],False)
+    next_url = url_for('explore',page=posts.next_num) if posts.has_next else None
+    prev_url = url_for('explore',page=posts.prev_num) if posts.has_prev else None
+    return render_template('index.html',title='Explore',posts=posts.items,next_url=next_url,prev_url=prev_url)
 
--- config.py
+++ config.py
@@ -6,4 +6,6 @@ class Config:
     SECRET_KEY = os.environ.get('SECRET_KEY') or 'you will never guess'
     SQLALCHEMY_DATABASE_URI = os.environ.get('DATABASE_URI') or 'sqlite:///' + os.path.join(base_dir,'app.db')
     SQLALCHEMY_TRACK_MODIFICATIONS = False
+    #每个页面帖子数量配置
+    POSTS_PER_PAGE = 3
 
